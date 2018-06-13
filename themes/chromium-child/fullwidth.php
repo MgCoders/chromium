954bcb1c81bccb1a21e7fde038b62f9cc8887ba3
goja288 <goja288@gmail.com>
2018-06-13 20:04:20 -0300

    equipo e imagenes

--- themes/chromium-child/fullwidth.php
+++ themes/chromium-child/fullwidth.php
@@ -9,6 +9,39 @@ get_header(); ?>
     </div><!-- .row -->
 </div><!-- .container -->
 
+    <div class="container-fluid team-container">
+    <div class="container text-center">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <img  src="<?php echo get_stylesheet_directory_uri(); ?>/img/team/01b.png" alt="Generic placeholder image" width="120" height="120">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3">
+                <img  src="<?php echo get_stylesheet_directory_uri(); ?>/img/team/02b.png" alt="Generic placeholder image" width="120" height="120">
+                <h2>Heading</h2>
+                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3">
+                <img  src="<?php echo get_stylesheet_directory_uri(); ?>/img/team/03b.png" alt="Generic placeholder image" width="120" height="120">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3">
+                <img  src="<?php echo get_stylesheet_directory_uri(); ?>/img/team/04b.png" alt="Generic placeholder image" width="120" height="120">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+    </div>
+    </div>
+
+
     <!-- stage 01 -->
     <div class="container-fluid stage-container">
     <div class="container">
