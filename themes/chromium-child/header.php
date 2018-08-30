1d85870f416ed74bed50286f7fb131a23d2117a2
goja288 <goja288@gmail.com>
2018-08-30 10:11:15 -0300

    language switch

--- themes/chromium-child/header.php
+++ themes/chromium-child/header.php
@@ -21,11 +21,13 @@
 </head>
 
 <body <?php body_class(); ?>>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-bootstrap-starter' ); ?></a>
     <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
 	<header id="masthead" class="site-header navbar-static-top <?php echo wp_bootstrap_starter_bg_class(); ?>" role="banner">
         <div class="container">
+            <div class="topcorner"> <?php pll_the_languages( array( 'show_flags' => 1,'show_names' => 0,'dropdown' => 0 ) ) ?></div>
             <nav class="navbar navbar-expand-xl p-0">
                 <div class="navbar-brand">
                     <?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
